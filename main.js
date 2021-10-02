a666483d3890389a554ce4b801aa4444684f3c17
funhousekush <85101915+funhousekush@users.noreply.github.com>
2021-10-02 16:52:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
+status = "";
+value = "";
 function preload(){}
 
 function setup()
 {
     canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    video.size(300, 300);
+}
+function start()
+{
+    objectdetection = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("statuss").innerHTML = "Status : Detecting objects";
+    value = document.getElementById("Texty_inputty").value;
+}
+function modelLoaded()
+{
+    console.log("model Has loaded");
+    status = true;
+}
+function draw()
+{
+    image(video, 0 , 0 , 300, 300);
 }
